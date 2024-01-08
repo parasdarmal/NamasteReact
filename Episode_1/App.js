c3882d08d06caf8a77b32ffd1452c3eaed361bb5
Paras Darmal <paras.darmal@globallogic.com>
2024-01-08 20:59:02 +0530

    Episode 1

---
+++ Episode_1/App.js
@@ -0,0 +1,13 @@
+//const heading = React.createElement("h1", {id: "heading"}, "hello world from React!");
+//empty object can be used to give attributes
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    React.createElement("div", {id: "child"},[
+        React.createElement("h1", {}, "I am first h1 tag"),
+        React.createElement("h1", {}, "I am second h1 tag")
+    ])
+    )
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
+root.render(parent);
